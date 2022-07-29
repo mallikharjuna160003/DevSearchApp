5a6f904727e121e6f91c77450b52d3dad03e6610
mallikharjuna160003 <sunkaramallikharjuna90@gmail.com>
2022-07-30 00:00:13 +0530

    authentication with styling

--- devsearch/users/forms.py
+++ devsearch/users/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,widgets
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,5 +8,11 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['first_name','email','username','password1','password2']
         labels ={
-            'first_name':'Name'
+            'first_name':'Name',
         }
+    def __init__(self,*args,**kwargs):
+
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
